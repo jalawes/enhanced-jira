9b05cdae78bd792b28f62d2687e08d26e14a0cb2
Wyatt Grant <wyatt.grant@coconutsoftware.com>
2019-07-16 12:14:40 -0600

    cleaned up setbackgroundimage.js

--- setBackgroundImage.js
+++ setBackgroundImage.js
@@ -1,26 +1,33 @@
 chrome.storage.sync.get('backgroundImageUrl', function(data) {
-    document.getElementById('jira').style.background = 'url(' + data.backgroundImageUrl + ')';
-    document.getElementById('gh').style.background = 'transparent';
-    document.getElementById('gh').style.backgroundColor = 'transparent';
-
-    if (document.getElementById('ghx-pool')) {
-        document.getElementById('ghx-pool').style.background = 'transparent';
-        document.getElementById('ghx-pool').style.backgroundColor = 'transparent';
-    }
-    if (document.getElementById('ghx-column-header-group')) {
-        document.getElementById('ghx-column-header-group').style.background = 'transparent';
-        document.getElementById('ghx-column-header-group').style.backgroundColor = 'transparent';
-    }
-    if (document.getElementById('jira-issue-container')) {
-        document.getElementById('jira-issue-container').style.background = '#ffffff';
-        document.getElementById('jira-issue-container').style.backgroundColor = '#ffffff';
-    }
-    if (document.getElementById('ghx-plan')) {
-        document.getElementById('ghx-plan').style.background = '#ffffff';
-        document.getElementById('ghx-plan').style.backgroundColor = '#ffffff';
-    }
-
     const collumns = document.getElementsByClassName('ghx-column ghx-narrow-card');
+    const gh = document.getElementById('gh');
+    const ghxColumn = document.getElementById('ghx-column-header-group');
+    const ghxPlan = document.getElementById('ghx-plan');
+    const ghxPool = document.getElementById('ghx-pool');
+    const jira = document.getElementById('jira');
+    const jiraIssueContainer = document.getElementById('ghx-column-header-group');
+
+    jira.style.background = 'url(' + data.backgroundImageUrl + ')';
+    gh.style.background = 'transparent';
+    gh.style.backgroundColor = 'transparent';
+
+    if (ghxPool) {
+        ghxPool.style.background = 'transparent';
+        ghxPool.style.backgroundColor = 'transparent';
+    }
+    if (ghxColumn) {
+        ghxColumn.style.background = 'transparent';
+        ghxColumn.style.backgroundColor = 'transparent';
+    }
+    if (jiraIssueContainer) {
+        jiraIssueContainer.style.background = '#ffffff';
+        jiraIssueContainer.style.backgroundColor = '#ffffff';
+    }
+    if (ghxPlan) {
+        ghxPlan.style.background = '#ffffff';
+        ghxPlan.style.backgroundColor = '#ffffff';
+    }
+
     for (let i = 0; i < collumns.length; i++) {
         collumns[i].style.background = 'rgba(255, 255, 255, 0.4)';
         collumns[i].style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
