1f32f62655013b2c4468a9fd451e7b0280115b6e
Wyatt Grant <wyatt.grant@coconutsoftware.com>
2019-07-16 14:04:26 -0600

    removed comment

--- background.js
+++ background.js
@@ -9,8 +9,6 @@ chrome.webNavigation.onCompleted.addListener(function(details){
     }
 });
 
-//sometimes data updates not on a page load, but after an ajax request.
-// this waits for requests for user data and resets the background image.
 chrome.webRequest.onBeforeRequest.addListener(function(details){
     if (details.url.includes('https://coconutcalendar.atlassian.net/rest/greenhopper/1.0/userData')) {
         chrome.tabs.executeScript(details.tabId, { file: "features/setBackgroundImage.js" });
