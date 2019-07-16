ee9440f831b21e0fa77c0bb6fc9bfac54605042e
Wyatt Grant <wyatt.grant@coconutsoftware.com>
2019-07-16 12:10:03 -0600

    added sort of hacky settimeout because executing background scipt was happening before all ajax calls finished when navigating between backlog and active sprint

--- background.js
+++ background.js
@@ -1,19 +1,11 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.storage.sync.set({backgroundImageUrl: 'https://picsum.photos/1920/1080'});
 });
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     if (changeInfo.status == 'complete') {
         if (tab.url.indexOf('coconutcalendar.atlassian.net') != -1) {
-            setBackgroundImage(tab);
+            setTimeout(function(){ chrome.tabs.executeScript(tab.id, { file: "setBackgroundImage.js" }); }, 1000);
         }
     }
 });
-
-
-function setBackgroundImage(tab) {
-    chrome.tabs.executeScript(tab.id, { file: "setBackgroundImage.js" });
-    // chrome.tabs.executeScript(tab.id, {code: 'document.body.style.background = "url(' + url + ')";'});
-    // chrome.tabs.executeScript(tab.id, {code: 'document.getElementById(\'gh\').style.background = "transparent";'});
-    // chrome.tabs.executeScript(tab.id, {code: 'document.getElementById(\'ghx-pool\').style.background = "transparent";'});
-    // chrome.tabs.executeScript(tab.id, {code: 'document.getElementById(\'ghx-column-header-group\').style.background = "transparent";'});
-}
--- setBackgroundImage.js
+++ setBackgroundImage.js
@@ -1,11 +1,8 @@
 chrome.storage.sync.get('backgroundImageUrl', function(data) {
-    if (document.getElementById('jira')) {
-        document.getElementById('jira').style.background = 'url(' + data.backgroundImageUrl + ')';
-    }
-    if (document.getElementById('gh')) {
-        document.getElementById('gh').style.background = 'transparent';
-        document.getElementById('gh').style.backgroundColor = 'transparent';
-    }
+    document.getElementById('jira').style.background = 'url(' + data.backgroundImageUrl + ')';
+    document.getElementById('gh').style.background = 'transparent';
+    document.getElementById('gh').style.backgroundColor = 'transparent';
+
     if (document.getElementById('ghx-pool')) {
         document.getElementById('ghx-pool').style.background = 'transparent';
         document.getElementById('ghx-pool').style.backgroundColor = 'transparent';
@@ -14,9 +11,17 @@ chrome.storage.sync.get('backgroundImageUrl', function(data) {
         document.getElementById('ghx-column-header-group').style.background = 'transparent';
         document.getElementById('ghx-column-header-group').style.backgroundColor = 'transparent';
     }
+    if (document.getElementById('jira-issue-container')) {
+        document.getElementById('jira-issue-container').style.background = '#ffffff';
+        document.getElementById('jira-issue-container').style.backgroundColor = '#ffffff';
+    }
+    if (document.getElementById('ghx-plan')) {
+        document.getElementById('ghx-plan').style.background = '#ffffff';
+        document.getElementById('ghx-plan').style.backgroundColor = '#ffffff';
+    }
 
-    var collumns = document.getElementsByClassName('ghx-column ghx-narrow-card');
-    for (var i = 0; i < collumns.length; i++) {
+    const collumns = document.getElementsByClassName('ghx-column ghx-narrow-card');
+    for (let i = 0; i < collumns.length; i++) {
         collumns[i].style.background = 'rgba(255, 255, 255, 0.4)';
         collumns[i].style.backgroundColor = 'rgba(255, 255, 255, 0.4)';
     }
