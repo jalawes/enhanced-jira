6527e4001e47afd3b965161ac17863c8a3a1b3c4
Wyatt Grant <wyatt.grant@coconutsoftware.com>
2019-07-16 14:04:16 -0600

    used webrequest to figure out when certain ajax requests are being made and then re-apply background

--- background.js
+++ background.js
@@ -8,3 +8,14 @@ chrome.webNavigation.onCompleted.addListener(function(details){
         chrome.tabs.executeScript(details.tabId, { file: "features/setBackgroundImage.js" });
     }
 });
+
+//sometimes data updates not on a page load, but after an ajax request.
+// this waits for requests for user data and resets the background image.
+chrome.webRequest.onBeforeRequest.addListener(function(details){
+    if (details.url.includes('https://coconutcalendar.atlassian.net/rest/greenhopper/1.0/userData')) {
+        chrome.tabs.executeScript(details.tabId, { file: "features/setBackgroundImage.js" });
+    }
+    return {requestHeaders: details.requestHeaders};
+},
+{urls: ["<all_urls>"]},
+["blocking"]);
