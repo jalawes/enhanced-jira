1b22aec3b28d2d35ec470b5e5d522881fb39793e
Wyatt Grant <wyatt.grant@coconutsoftware.com>
2019-07-16 11:04:41 -0600

    removed content.js I was messing around with

--- content.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.text && (msg.text == "change_the_background")) {
-        sendResponse(document.innerHTML);
-    }
-});
