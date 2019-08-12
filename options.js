1e6bafe34be33d50fae425a60dbb8dda09586f49
Jamal Alawes <jamal.alawes@gmail.com>
2019-08-11 17:18:34 -0700

    updates js

--- options.js
+++ options.js
@@ -1,10 +1,23 @@
 const saveButton = document.getElementById('optionsSave');
-saveButton.addEventListener('click', () => {
+
+const successAlert = document.getElementById('success-alert');
+
+const showSaveAlert = () => {
+  successAlert.classList.remove('hidden');
+  window.setTimeout(() => {
+    successAlert.classList.add('hidden');
+  }, 5000);
+};
+
+const save = () => {
   const enableCustomBackground = document.getElementById('optionEnableCustomBackground');
   const customBackgroundUrl = document.getElementById('optionCustomBackgroundUrl');
   chrome.storage.sync.set({ backgroundEnabled: enableCustomBackground.checked });
   chrome.storage.sync.set({ backgroundImageUrl: customBackgroundUrl.value });
-});
+  showSaveAlert();
+};
+
+saveButton.addEventListener('click', () => save());
 
 document.addEventListener('DOMContentLoaded', () => {
   chrome.storage.sync.get(['backgroundImageUrl', 'backgroundEnabled'], (data) => {
