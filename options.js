70b75e847dd6a3e64bf1d9728e4b1de69bc97fd9
Wyatt Grant <wyatt.grant@coconutsoftware.com>
2019-08-20 11:22:22 -0600

    hihglighting is now functional

--- options.js
+++ options.js
@@ -1,32 +1,33 @@
 document.addEventListener("DOMContentLoaded", function() {
+    //save
     const showSaveAlert = () => {
         const successAlert = document.getElementById('success-alert');
         successAlert.classList.remove('hidden');
         window.setTimeout(() => {
             successAlert.classList.add('hidden');
         }, 5000);
-        showSaveAlert();
     };
-
     const save = () => {
         const enableCustomBackground = document.getElementById('optionEnableCustomBackground');
         const customBackgroundUrl = document.getElementById('optionCustomBackgroundUrl');
+        const enableAssigneeHighlighting = document.getElementById('optionEnableAssigneeHighlighting');
         const assigneeNames = document.querySelectorAll('.optionAssignee');
         const assigneeColours = document.querySelectorAll('.optionAssigneeColour');
-        assigneeNameAndColours = {};
+        let assigneeNameAndColours = {};
         for (let i = 0; i < assigneeNames.length; i++) {
             assigneeNameAndColours[assigneeNames[i].value] = assigneeColours[i].value;
         }
 
         chrome.storage.sync.set({backgroundEnabled: enableCustomBackground.checked});
         chrome.storage.sync.set({backgroundImageUrl: customBackgroundUrl.value});
+        chrome.storage.sync.set({assigneeHighlightingEnabled: enableAssigneeHighlighting.checked});
         chrome.storage.sync.set({assigneeNameAndColours: assigneeNameAndColours});
         showSaveAlert();
     };
-
     document.getElementById('optionsSave').addEventListener('click', () => save());
 
-    const removeAssignee = function (event) {
+    //remove assignee option settings
+    const removeAssignee = (event) => {
         const assignee = event.target.parentNode.parentNode;
         assignee.parentNode.removeChild(assignee);
     };
@@ -35,6 +36,7 @@ document.addEventListener("DOMContentLoaded", function() {
         removeAssigneeButtons[i].addEventListener('click', removeAssignee, false);
     }
 
+    //add assignee option settings
     const assigneeHTML = `<div class="md:flex md:items-center mb-1">
           <div class="md:w-1/3">
             <label class="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4">
@@ -47,8 +49,7 @@ document.addEventListener("DOMContentLoaded", function() {
             <button class="mt-1 shadow bg-red-500 hover:bg-red-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded removeAssigneeButton" type="button">remove</button>
           </div>
         </div>`;
-
-    function addAssignee(name, colour) {
+    const addAssignee = (name, colour) => {
         const assignee = document.createElement("div");
         assignee.innerHTML = assigneeHTML;
         assignee.querySelector('.optionAssignee').value = name;
@@ -56,23 +57,21 @@ document.addEventListener("DOMContentLoaded", function() {
         const parentContainer = document.querySelector('form');
         assignee.querySelector('.removeAssigneeButton').addEventListener('click', removeAssignee, false);
         parentContainer.insertBefore(assignee, parentContainer.children[5]);
-    }
+    };
+    document.getElementById('addAssignee').addEventListener('click', () => addAssignee("", ""));
 
-    const addAssigneeButton = document.getElementById('addAssignee');
-    addAssigneeButton.addEventListener('click', function (event) {
-        addAssignee("", "");
-    });
-
-
-    function assigneeNameAndColours(assigneeNameAndColours) {
-        Object.keys(assigneeNameAndColours).forEach(function(key) {
-            addAssignee(key, assigneeNameAndColours[key]);
-        });
-    }
-
-    chrome.storage.sync.get(['backgroundImageUrl', 'backgroundEnabled', 'assigneeNameAndColours'], function(data) {
+    //load/set previous settings
+    const assigneeNameAndColours = (assigneeNameAndColours) => {
+        if (assigneeNameAndColours) {
+            Object.keys(assigneeNameAndColours).forEach(function(key) {
+                addAssignee(key, assigneeNameAndColours[key]);
+            });
+        }
+    };
+    chrome.storage.sync.get(['backgroundImageUrl', 'backgroundEnabled', 'assigneeHighlightingEnabled', 'assigneeNameAndColours'], function(data) {
         document.getElementById('optionEnableCustomBackground').checked = data.backgroundEnabled;
         document.getElementById('optionCustomBackgroundUrl').value = data.backgroundImageUrl;
+        document.getElementById('optionEnableAssigneeHighlighting').checked = data.assigneeHighlightingEnabled;
         assigneeNameAndColours(data.assigneeNameAndColours);
     });
 });
